9787f59c2c0f944b7ad4734f55b6673e10fa7669
Tom Dezentje <tomdezentje@gmail.com>
2020-03-30 20:21:34 +0200

    updated build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,6 @@
 const webpack = require('webpack');
 const baseConfig = require('./webpack.config');
 const merge = require('webpack-merge');
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-const runtimeCaching = require('./webpack.swcache');
 
 const common = {
     mode: 'production',
